94639787a95a9cdc88ddf4835456899684b24ecb
superpowerranger <55907963+superpowerranger@users.noreply.github.com>
2020-12-27 16:24:25 +0530

    changes

--- index.php
+++ index.php
@@ -19,7 +19,47 @@
   </head> 
   <body>
     <!-- Start header --> 
-    <?php include 'header.php';?>
+    <header id="top-header" class="site-header"> 
+      <nav class="navbar px-md-3 px-2 px-md-0 py-lg-0 py-3 navbar-light" id="navbar-example2" >
+        <a class="navbar-brand py-1 pl-3" href="index.php">
+          <img src="assets/images/logo.png" alt="logo" class="logo img-fluid">
+        </a>
+        <div class="navbar-collapse offcanvas-collapse" id="offcanvassidebar">
+          <ul class="navbar-nav ml-auto" id="mainmenu">
+            <li class="nav-item">
+              <a class="nav-lik" data-toggle="offcanvas" href="about-us.php">About Us</a>
+            </li>
+            <li class="nav-ite">
+              <a class="nav-link" data-toggle="offcanvas" href="#">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="offcanvas" href="portfolio.php">Our Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="offcanvas" href="#">Ready to Use</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="offcanvas" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="offcanvas" href="career.php">Career</a>
+            </li>
+          </ul>
+        </div>
+        <div class="nav-btns nav-item d-none d-max-xsmall-block d-sm-block ml-auto">
+            <a href="contact-us.php" class="btn px-4">
+              <span class="">list a warehouse</span>
+            </a>
+            <a href="contact-us.php" class="btn px-4">
+              <span class="">sign in</span>
+            </a>
+        </div>
+        <button class="navbar-toggler border-0" type="button" data-toggle="offcanvas" data-target="offcanvassidebar" id="offcanvassidebar-btn">
+          <!-- <i class="fas fa-bars"></i> -->
+          <span></span>
+        </button>
+      </nav>
+    </header>
     <!-- end of header --> 
 
     <main class="page-wrapper pt-0">
@@ -355,4 +395,158 @@
 
     </main>
 
-<?php include 'footer.php';?>
+
+    <footer class="pb-1 section">
+  <div class="container pt-5">
+    <div class="row pb-3 ">
+      <div class="col-lg-auto col-sm-6 pb-3 pb-md-0 mb-4"><a class="d-block mb-4" href="index.html" style="width: 153px;"><img src="assets/images/logo.png" alt="logo" class="img-fluid w-100"></a>
+        <!-- <div class="mb-4">
+          <h5 class="text-white">Contact Number</h5>
+          <a href="tel:+91-8178045778" class="text-gray">+91-8178045778</a>
+        </div>
+        <div class="mb-4">
+          <h5 class="text-white">Email Us</h5>
+          <a href="mailto:contact@escalesolutions.com" class="text-gray">contact@escalesolutions.com</a>
+        </div>
+        <div class="pt-5">
+          <img src="assets/images/footer-image.png" alt="image" class="w-250px">
+        </div> -->
+        
+      </div>
+      <div class="col-xl col-lg-4 col-sm-6 pb-1 mb-4">
+        <h6 class="pl-3 mb-4 text-uppercase">About us</h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="service.php">New User</a>
+          </li> 
+        </ul>
+      </div>
+      <div class="col-xl col-lg-4 col-sm-6 pb-1 mb-4">
+        <h6 class="pl-3 mb-4 text-uppercase">Important links</h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="#">List a Warehouse</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Testimonial</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Support</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Terms & Condition</a>
+          </li> 
+        </ul>
+      </div> 
+      <div class="col-xl col-lg-4 col-sm-6 pb-1 mb-4">
+        <h6 class="pl-3 mb-4 text-uppercase">Need Help?</h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="#">FAQs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="service.php">Feedback</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tutorials</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li> 
+        </ul>
+      </div>
+      <div class="col-xl col-lg-4 col-sm-6 pb-1 mb-4">
+        <div class="pl-3">
+          <h6 class="mb-4 text-uppercase">Follow Us</h6>
+          <div class="social-icons bg-socials row align-items-center">
+            <div class="col-auto px-1-12">
+              <a href="#" target="_blank" class="facebook d-flex align-items-center justify-content-center">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </div>
+            <div class="col-auto px-1">
+              <a href="" target="_blank" class="twitter d-flex align-items-center justify-content-center">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </div>
+            <div class="col-auto px-1">
+              <a href="#" target="_blank" class="youtube d-flex align-items-center justify-content-center">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </div> 
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="row align-items-center pt-1 mx-0 border-top">
+      <div class="order-md-1 py-3">
+        <p class="mb-0 text-gray"><span class=" mr-1">© 2020 All rights reserved | Site maintained by <a class="text-gray" href="#" target="_blank" rel="noopener">Escale Solutions</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- <button class="btn message-btn" data-toggle="modal" data-target="#quote-modal">
+  <img src="assets/images/icons/icon-message-primary.png" alt="images">
+</button> -->
+
+<!-- Modal -->
+<div class="modal fade" id="quote-modal" tabindex="-1" role="dialog" aria-labelledby="quote-modalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <i class="fas fa-times"></i>
+        </button>
+      </div>
+        <div class="ml-4 pl-3">
+        <h4 class="modal-title pl-1" id="quote-modalLongTitle">Send us your requirements</h5>
+        </div>
+      <div class="modal-body">
+        <div class="mx-auto mb-md-0 mb-5">
+          <div class="card rounded-md px-2 pb-4 banner-card contact-form">
+            <div class="card-body">
+              <form action="">
+                <div class="row">
+                  <div class="col-md-6 form-group pb-2">
+                    <label for="">Your name <sup class="text-danger">*</sup></label>
+                    <input type="text" id="username" class="form-control bg-light-primary bg-light-primary" placeholder="First name">
+                  </div>
+                  <div class="col-md-6 form-group pb-2">
+                    <label for="">Company name <sup class="text-danger">*</sup></label>
+                    <input type="text" id="companyname" class="form-control bg-light-primary" placeholder="Last name">
+                  </div>
+                  <div class="col-md-6 form-group pb-2">
+                    <label for="">Email ID <sup class="text-danger">*</sup></label>
+                    <input type="text" id="companyname" class="form-control bg-light-primary" placeholder="Email id">
+                  </div>
+                  <div class="col-md-6 form-group pb-2">
+                    <label for="">Phone number <sup class="text-danger">*</sup></label>
+                    <input type="text" id="companyname" class="form-control bg-light-primary" placeholder="Phone number">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Example textarea</label>
+                  <textarea class="form-control bg-light-primary" id="exampleFormControlTextarea1" placeholder="Description*" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary my-3">Submit</button>
+              </form>
+              <p class="text-muted small mb-0">*Create your own <span class="text-primary">free form with HubSpot</span>.</p>
+            </span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="assets/js/jquery.min.js"></script> 
+    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>  
+    <script src="assets/plugins/owl-carousel/js/owl.carousel.js"></script>  
+    <script src="assets/plugins/slick/slick.min.js"></script>  
+    <script src="assets/js/custom.min.js"></script>
+  </body>
+</html>
