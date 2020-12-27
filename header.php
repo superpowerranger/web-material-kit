37125e96d9fd5960ee1726f3597d84bdb7d5c567
superpowerranger <55907963+superpowerranger@users.noreply.github.com>
2020-12-27 13:31:48 +0530

    initial upload

---
+++ header.php
@@ -0,0 +1,42 @@
+
+<header id="top-header" class="site-header"> 
+  <nav class="navbar px-md-3 px-2 px-md-0 py-lg-0 py-3 navbar-light" id="navbar-example2" >
+    <a class="navbar-brand py-1 pl-3" href="index.php">
+      <img src="assets/images/logo.png" alt="logo" class="logo img-fluid">
+    </a>
+    <div class="navbar-collapse offcanvas-collapse" id="offcanvassidebar">
+      <ul class="navbar-nav ml-auto" id="mainmenu">
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="offcanvas" href="about-us.php">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="offcanvas" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="offcanvas" href="portfolio.php">Our Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="offcanvas" href="#">Ready to Use</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="offcanvas" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="offcanvas" href="career.php">Career</a>
+        </li>
+      </ul>
+    </div>
+    <div class="nav-btns nav-item d-none d-max-xsmall-block d-sm-block ml-auto">
+        <a href="contact-us.php" class="btn px-4">
+          <span class="">list a warehouse</span>
+        </a>
+        <a href="contact-us.php" class="btn px-4">
+          <span class="">sign in</span>
+        </a>
+    </div>
+    <button class="navbar-toggler border-0" type="button" data-toggle="offcanvas" data-target="offcanvassidebar" id="offcanvassidebar-btn">
+      <!-- <i class="fas fa-bars"></i> -->
+      <span></span>
+    </button>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.jpeg" />
+
+    <title>Escale Solutions</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">  
+  </head> 
+  <body>
+    <!-- Start header --> 
+    <?php include 'header.php';?>
+    <!-- end of header --> 
+
+    <main class="page-wrapper pt-0">
+      <section class="banner pb-0">
+        <div id="carouselExampleCaptions" class="carousel carousel-fade slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="carousel-caption">
+                <div class="container">
+                  <div class="row align-items-center align-items-lg-start mt-5 pt-xl-5">
+                    <div class="py-2 mx-auto carousel-caption-text text-center">
+                      <h1 class="display-4 text-white mb-md-4">Fulfilment Warehousing Digitally Delivered</h1>
+                      <div class="col-lg-10 mx-auto mb-4">
+                        <h3 class="h3 text-white font-weight-light">Select a warehouse from a large and growing network of functional warehouses across the country!</h3>
+                      </div>
+                      <a href="#" class="btn border-white text-white hover-border-deep-primary hover-text-deep-primary hover-bg-white">Explore More</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- CAROUSEL END --> 
+
+        <div class="bg-overlay"></div>
+      </section>
+      <section class="filter-section pt-0">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-xxl-8 col-xl-10 col-lg-11 col-md-12 col-sm-7 shadow bg-white filters">
+              <form>
+                <div class="row align-items-center">
+                  <div class="col-md-3 px-0 border-md-right border-max-md-bottom border-deep-gray">
+                    <div class="form-group mb-0">
+                      <div class="input-group input-group-lg py-2 px-3 custom-input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text px-1"><img src="assets/images/icons/location-dark.png" alt="image"></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Location">
+                      </div>
+                    </div> 
+                  </div>
+                  <div class="col-md-3 px-0 border-md-right border-max-md-bottom border-deep-gray">
+                    <div class="form-group mb-0">
+                      <div class="input-group input-group-lg py-2 px-3 custom-input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text px-1"><img src="assets/images/icons/space-dark.png" alt="image"></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Space Required">
+                      </div>
+                    </div> 
+                  </div>
+                  <div class="col-md-3 pl-0 border-md-right border-max-md-bottom border-deep-gray">
+                    <div class="form-group mb-0">
+                      <div class="input-group input-group-lg py-2 px-3 custom-input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text px-1"><img src="assets/images/icons/pallet-dark.png" alt="image"></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="No. of Pallets">
+                      </div>
+                    </div> 
+                  </div>
+                  <div class="col-md-3 px-0">
+                    <div class="btn-group mb-0 btn-group-lg w-100">
+                      <button type="submit" class="btn btn-lg py-3">
+                        <i class="fas fa-search"> </i>
+                      </button>
+                      <a href="#" class="btn btn-lg btn-dark-primary d-flex align-items-center rounded-0"><span class="small d-flex align-items-center"><img src="assets/images/icons/filter-white.png" alt="image"> Advanced Filter</span></a>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row section-heading text-center">
+            <div class="col-12">
+              <p class="text-primary text-uppercase my-4 pt-4">Welcome Warehousity</p>
+              <h2 class="h1 main-heading">Browse or Book Warehouses around India</h2>
+            </div>
+          </div>
+          <div class="row align-items-center-align-items-center">
+
+          </div>
+        </div>
+      </section>
+
+      <section class="four-carousel" id="four-carousel1">
+        <div class="container-fluid">
+          <div class="row section-heading text-center">
+            <div class="col-12">
+              <h2 class="h1 main-heading">Featured Warehouses</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xl-9 col-lg-8 col-md-7 col-sm-5 four-element-slider order-sm-2">
+              <div class="four-element-carousel">
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product1.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product2.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product3.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product2.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product1.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product2.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7  d-flex bg-dark-primary order-sm-1 align-items-end justify-content-center">
+              <div class="four-element-carousel-counter" data-carousel-target=".four-element-carousel" data-carousel-parent="#four-carousel1" id="four-carousel-counter1">
+                <div class="counter d-flex align-items-end mb-5">
+                  <div class="current-item counter-item fa-4x"></div> 
+                  <div class="total-item counter-item fa-2x"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="warehouse-banner bg-deep-gray">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="img-holder col-lg-8 mx-auto"><img src="assets/images/warehouse-clipart.png" alt="image" class="img-fluid w-100"></div>
+            </div>
+            <div class="col-md-6 section-heading">
+              <p class="text-gray mb-2">rent your property</p>
+              <h4 class="mb-3">List your warehouse for free</h4>
+              <div class="mb-4">
+                <ul class="pl-3 text-gray">
+                  <li class="mb-2">List your warehouse hassle free.</li>
+                  <li class="mb-2">Great Deals</li>
+                  <li class="mb-2">Easy monthly/yearly payments</li>
+                </ul>
+              </div>
+              <div class="">
+                <a href="#" class="btn btn-deep-primary">Get Started</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="four-carousel" id="four-carousel2">
+        <div class="container-fluid">
+          <div class="row section-heading text-center">
+            <div class="col-12">
+              <h2 class="h1 main-heading">Types of Warehouses</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xl-9 col-lg-8 col-md-7 col-sm-5 four-element-slider order-sm-2">
+              <div class="four-element-carousel">
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product1.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product2.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product3.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product2.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product1.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+                <div class="card custom-card custom-shadow my-sm-5 my-4 mx-3">
+                  <div class="img-holder">
+                    <img src="assets/images/product/product2.png" alt="image" class="img-fluid w-100">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">ID: DL-01379</h5>
+                    <p class="card-text"><i class="fas fa-map-marker pr-1 size-20px"></i>Gurgaon, Haryana</p>
+                    <p class="card-text"><span class="d-inline-block"><img src="assets/images/icons/icon-area-dark.png" alt="image" class=""></span> Gurgaon, Haryana</p>
+                  </div>
+                  <div class="card-footer btn-bottom">
+                    <a href="#" class="btn btn-dark btn-block">View Details</a>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7  d-flex bg-dark-primary order-sm-1 align-items-end justify-content-center">
+              <div class="four-element-carousel-counter" data-carousel-target=".four-element-carousel" data-carousel-parent="#four-carousel2" id="four-carousel-counter2">
+                <div class="counter d-flex align-items-end mb-5">
+                  <div class="current-item counter-item fa-4x"></div> 
+                  <div class="total-item counter-item fa-2x"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </main>
+
+<?php include 'footer.php';?>
